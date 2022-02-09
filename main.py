cedf3153a477c1182989aabc3608820c83f66d67
epsatya <caspairuserbot@gmail.com>
2022-02-09 15:37:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from pyrogram import Client, filters
 from time import sleep
-
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 plugins = dict(
     root="plugins"
 )
@@ -13,8 +13,16 @@ Bot = Client(
     api_hash = "fd7acd07303760c52dcc0ed8b2f73086",
     plugins=plugins,
 )
+from datetime import date, datetime
+
+toda = date.today()
+today = toda.strftime("%b-%d-%Y")
+print(today)
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
 with Bot:
-    Bot.send_message("@ourclg", "Im started..")
+    Bot.send_message("@ourclg", "Im started.."+"\n"+today+"\n"+current_time, reply_markup=InlineKeyboardMarkup(text='CHECK STATUS', callback_data='amialive'))
 @Bot.on_message(filters.command(["restart"]))
 async def restart(c, m):
     k=await m.reply_text("🔄 **Restarting...**")
@@ -22,7 +30,7 @@ async def restart(c, m):
     await k.edit("🔄 **Restarting, Please Wait...**")
     sleep(1)
     await k.edit("**Restarted**")
-    await Bot.send_message("@ourclg", "Im started..")
+    await Bot.send_message("@ourclg", "Im started.."+"\n"+today+"\n"+current_time, reply_markup=InlineKeyboardMarkup(text='CHECK STATUS', callback_data='amialive'))
 
 print("starting..raaa")
 Bot.run()
