91c38ce7f6263fb1eb4db3a9ef7bcd804e76ca1d
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2022-02-10 12:53:38 +0530

    Update hw.py

--- plugins/hw.py
+++ plugins/hw.py
@@ -1,5 +1,26 @@
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import *
+import requests
+import re
+import urllib
+import urllib.parse
+import sys
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import Client as Bot, filters
-from pyrogram import filters 
-@Bot.send_message(filters.command("che"))
-async def che(c, m):
-  await m.reply_text("YOU ARE NOT  AUTHORISED!")
+import re
+import time 
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+from pyrogram import Client, filters
+from telegraph import upload_file as uf
+import requests
+from bs4 import BeautifulSoup
+
+@Bot.on_message(filters.command(["che"]))
+async def che(bot, m):
+   
+        await m.reply_text(
+        text=f"YOU ARE NOTAUTHORISED"
+    )
