e3ce35d3696202619a3819743a029a1fe9c86640
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2022-02-10 12:34:52 +0530

    Update hw.py

--- plugins/hw.py
+++ plugins/hw.py
@@ -1,4 +1,5 @@
 from pyrogram import Client as Bot, filters
+from pyrogram import filters 
 @Bot.send_message(filters.command("che"))
 async def che(c, m):
   await m.reply_text("YOU ARE NOT  AUTHORISED!")
