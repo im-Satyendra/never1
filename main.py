1c83c6c86c49bff48e8f9ddd34964d13e4449857
epsatya <caspairuserbot@gmail.com>
2022-02-12 22:38:07 +0530

    u

--- main.py
+++ main.py
@@ -2,6 +2,30 @@
 from pyrogram import Client, filters
 from time import sleep
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import logging
+from tglogging import TelegramLogHandler
+
+# TelegramLogHandler is a custom handler which is inherited from an existing handler. ie, StreamHandler.
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        TelegramLogHandler(
+            token="5016166355:AAEKxdlkF7ofJNW_bHKZRxl1iRHZlaR519A", 
+            log_chat_id=-1001543238877, 
+            update_interval=2, 
+            minimum_lines=1, 
+            pending_logs=200000),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+logger.info("live log streaming to telegram.")
+
 plugins = dict(
     root="plugins"
 )
