ad7afa9006800989a38f8773bd9a21af50540129
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2022-02-13 16:39:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,1 @@
-echo "Cloning Repo, Please Wait..."
-git clone https://github.com/casperTeam/never1.git /never1
-echo "Installing Requirements..."
-cd /never1
-pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
 python3 main.py
