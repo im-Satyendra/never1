1c7924ad74d1b94b9aeec355af8f48cefc3f7996
SaTyEndRa <84174959+S4TyEndRa@users.noreply.github.com>
2022-07-11 21:15:01 +0530

    update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ plugins = dict(
 
 Bot = Client(
     "bot",
-    bot_token = "5016166355:AAEKxdlkF7ofJNW_bHKZRxl1iRHZlaR519A",
+    bot_token = "5526290298:AAEobpFpHx8MEp2jkttD_j4z9XCnLTeYAlQ",
     api_id = 2171111,
     api_hash = "fd7acd07303760c52dcc0ed8b2f73086",
     plugins=plugins,
@@ -24,21 +24,20 @@ datetime_ist = datetime.now(IST)
 dt = datetime_ist.strftime('%Y:%m:%d %H:%M:%S')
 print(dt)
 with Bot:
-    Bot.send_message("@ourclg", "Im started.."+"\n"+dt,reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(text='CHECK STATUS', callback_data='alive')]]))
+    Bot.send_message(-1001497428213, "Im started.."+"\n"+dt,reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(text='CHECK STATUS', callback_data='alive')]]))
 @Bot.on_message(filters.command(["restart"]))
-async def restart(c, m):
-    k=await m.reply_text("🔄 **Restarting...**")
-    sleep(1)
-    await k.edit("**cloning repo...**")
-    os.system("git clone https://github.com/casperTeam/never1.git")
-    sleep(1)
-    await k.edit("entering dir")
-    sleep(2)
-    os.system("cd never1 && python3 main.py")
-    await k.edit("🔄 **Starting..bash by python3 main.py**")
-    sleep(1)
-    await k.edit("**Restarted**")
-    await Bot.send_message("@ourclg", "Im started.."+"\n"+dt, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(text='CHECK STATUS', callback_data='alive')]]))
+async def restart(_, message):
+    m = message
+    id = m.from_user.id
+    if id != 1089528685:
+        await m.delete()
+        time.sleep(3)
+        mm = await m.reply_sticker(
+            "CAACAgUAAx0CYF-hHQACVotid_YH5QunOUMFwF2C1HwkWEjGNAAC6wIAAsYNkFeYvtc3bIYTMB4E"
+        )
+        await m.reply_text(
+            "contact @s4tyendra to use this bot\nor wait a week! and dont block me"
+        )
 
 print("starting..raaa")
 Bot.run()
